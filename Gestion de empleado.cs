8e1f8a0fd34d92def260e2b0da2e3c9698431271
Ervin Enmanuel Molina Montoya <168238700+Montoya366@users.noreply.github.com>
2024-09-09 17:02:11 -0600

    Add files via upload

---
+++ Gestion de empleado.cs
@@ -0,0 +1,38 @@
+namespace GestionDeEmpleados2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            label1.Text = txtNombre.Text;
+            la
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
